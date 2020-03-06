09baeecce22a1846af88e2235b1767bdd387d506
BrookSmith44 <brook.smith.182@gmail.com>
2020-03-06 00:37:45 +0000

    Add button to the form to get the dates, create list, create connection and open connection, query to get dates from the tables, add results to the list

--- IMAT2214_1819_502_Assignment_2/IMAT2214_1819_502_Assignment_2/Form1.Designer.cs
+++ IMAT2214_1819_502_Assignment_2/IMAT2214_1819_502_Assignment_2/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGetDates = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetDates
+            // 
+            this.btnGetDates.Location = new System.Drawing.Point(12, 26);
+            this.btnGetDates.Name = "btnGetDates";
+            this.btnGetDates.Size = new System.Drawing.Size(75, 23);
+            this.btnGetDates.TabIndex = 0;
+            this.btnGetDates.Text = "Get Dates";
+            this.btnGetDates.UseVisualStyleBackColor = true;
+            this.btnGetDates.Click += new System.EventHandler(this.btnGetDates_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGetDates);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGetDates;
     }
 }
 
--- IMAT2214_1819_502_Assignment_2/IMAT2214_1819_502_Assignment_2/Form1.cs
+++ IMAT2214_1819_502_Assignment_2/IMAT2214_1819_502_Assignment_2/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,40 @@ namespace IMAT2214_1819_502_Assignment_2
         public Form1()
         {
             InitializeComponent();
+        }
 
+        private void btnGetDates_Click(object sender, EventArgs e)
+        {
+            // Create list for the dates to be collected in
+            List<string> Dates = new List<string>();
+
+            // Create the database string
+            string connectionString = Properties.Settings.Default.Data_set_1ConnectionString;
+
+            // Create a boundary for the object to be used - Object will be destroyed at the end of te block
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                // Opens a filestream to the specified path
+                connection.Open();
+                // Reader provides a way of reading forward only stream of data rows from a data source
+                OleDbDataReader reader = null;
+                // getDates allows us to write a query in order to get the data from the rows we want
+                OleDbCommand getDates = new OleDbCommand("SELECT [Order Date], [Ship Date] FROM Sheet1", connection);
+
+                // Sends the command query to the reader
+                reader = getDates.ExecuteReader();
+
+                // Call read before accessing data - while will keep reading until there are no results left
+                while (reader.Read())
+                {
+                    // Add date to the list from the first column
+                    Dates.Add(reader[0].ToString());
+                    // Add date to the list from the second colun
+                    Dates.Add(reader[1].ToString());
+                }
+                // Display the populated list in the console window
+                Dates.ForEach(Console.WriteLine);
+            }
         }
     }
 }
