dbcb2ad4b6438b1e07cda35d6928d5b9fc91254b
BrookSmith44 <brook.smith.182@gmail.com>
2020-04-18 20:44:05 +0100

    Added second dataset

--- IMAT2214_1819_502_Assignment_2/IMAT2214_1819_502_Assignment_2/Properties/Settings.Designer.cs
+++ IMAT2214_1819_502_Assignment_2/IMAT2214_1819_502_Assignment_2/Properties/Settings.Designer.cs
@@ -44,5 +44,16 @@ namespace IMAT2214_1819_502_Assignment_2.Properties {
                 return ((string)(this["DestinationDatabaseConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"|DataDirectory|\\External Files\\Dat" +
+            "aSet2(1).accdb\"")]
+        public string DataSet2_1_ConnectionString {
+            get {
+                return ((string)(this["DataSet2_1_ConnectionString"]));
+            }
+        }
     }
 }
